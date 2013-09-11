19b79b79e032ba460a805c8f29fa700d6abef5e4
Jesse <lampposteffect@live.com>
2013-09-11 11:24:12 -0500

    Initial commit of code

---
+++ Comments/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Comments
+{
+    partial class Comments
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtComments = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtComments
+            // 
+            this.txtComments.Location = new System.Drawing.Point(1, 0);
+            this.txtComments.Margin = new System.Windows.Forms.Padding(0);
+            this.txtComments.Name = "txtComments";
+            this.txtComments.Size = new System.Drawing.Size(293, 20);
+            this.txtComments.TabIndex = 0;
+            this.txtComments.TextAlignChanged += new System.EventHandler(this.Comments_MouseEnter);
+            this.txtComments.TextChanged += new System.EventHandler(this.Comments_MouseEnter);
+            this.txtComments.MouseEnter += new System.EventHandler(this.Comments_MouseEnter);
+            this.txtComments.MouseLeave += new System.EventHandler(this.Comments_MouseLeave);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(290, 16);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(1, 1);
+            this.btnSubmit.TabIndex = 1;
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Comments
+            // 
+            this.AcceptButton = this.btnSubmit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 20);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtComments);
+            this.MaximizeBox = false;
+            this.Name = "Comments";
+            this.Opacity = 0.15D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Comments";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Comments_FormClosing);
+            this.Load += new System.EventHandler(this.Comments_Load);
+            this.MouseEnter += new System.EventHandler(this.Comments_MouseEnter);
+            this.MouseLeave += new System.EventHandler(this.Comments_MouseLeave);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtComments;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ Comments/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Comments
+{
+    public partial class Comments : Form
+    {
+        enum Direction { Up, Down, Left, Right, Nondirectional };
+        string _todaysLog = Path.Combine(Properties.Settings.Default.CommentLogLocation, getCommentLogName(DateTime.Now));
+        static bool _isFirstRun = true;
+
+        public Comments()
+        {
+            InitializeComponent();
+        }
+
+        private void Comments_Load(object sender, EventArgs e)
+        {
+            //Position textbox
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Top = Screen.PrimaryScreen.WorkingArea.Height - this.Height;
+            this.Left = (Screen.PrimaryScreen.WorkingArea.Width/2 - this.Width) + 400;
+
+            //If current file doesn't exist this is probably the first start up of the day.
+            if (!File.Exists(_todaysLog))
+                displayPreviousDayLog();
+
+            if (_isFirstRun)
+            {
+                InsertLog("*Application Start*");
+                _isFirstRun = false;
+            }
+
+            txtComments.Text = default(string);
+        }
+
+        private static string getCommentLogName(DateTime date)
+        {
+            return date.ToString("yyyyMMdd") + ".txt";
+        }
+
+        private void ShowTodaysLog()
+        {
+            if (!File.Exists(_todaysLog))
+                txtComments.Text = "No log for today.";
+            else
+                System.Diagnostics.Process.Start("notepad++.exe", _todaysLog);
+        }
+
+        private void ShowAllLogs()
+        {
+            if (!Directory.Exists(Properties.Settings.Default.CommentLogLocation))
+                Directory.CreateDirectory(Properties.Settings.Default.CommentLogLocation);
+
+            System.Diagnostics.Process.Start("explorer.exe", Properties.Settings.Default.CommentLogLocation);
+        }
+
+        private void Comments_MouseEnter(object sender, EventArgs e)
+        {
+            this.Opacity = 1.0;
+        }
+
+        private void Comments_MouseLeave(object sender, EventArgs e)
+        {
+            this.Opacity = 0.15;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string messageText = string.Format(@"/exit, /e, or /x to Exit Application. {0}/todayslog or /t for Today's Log. {0}/logs or /l for Log Directory. {0}/yolo or /y for Yesterday's Log. {0}/help or /h to Display This Again.", Environment.NewLine);
+            if (!String.IsNullOrWhiteSpace(txtComments.Text))
+            {
+                var comment = txtComments.Text.Trim();
+                Direction direction = directionTranslator(comment);
+
+                if (comment == "/exit" || comment == "/e" || comment == "/x")
+                    Application.Exit();
+                else if (comment == "/todayslog" || comment == "/t")
+                    ShowTodaysLog();
+                else if (comment == "/logs" || comment == "/l")
+                    ShowAllLogs();
+                else if (comment == "/help" || comment == "/h")
+                    MessageBox.Show(messageText, "Help Menu");
+                else if (comment == "/yolo" || comment == "/y" || comment == "/yesterday")
+                    displayPreviousDayLog();
+                else if (direction != Direction.Nondirectional)
+                    changeFormPosition(direction, comment);
+                else
+                    InsertLog(comment);
+
+                txtComments.Text = string.Empty;
+                txtComments.Focus();
+            }
+        }
+
+        private void changeFormPosition(Direction direction, string comment)
+        {
+            int numberOfPixels = Int32.Parse(Regex.Match(comment, @"\d+$").Value);
+
+            if (direction == Direction.Up)
+                this.Top = this.Top - numberOfPixels;
+            else if (direction == Direction.Right)
+                this.Left = this.Left + numberOfPixels;
+            else if (direction == Direction.Left)
+                this.Left = this.Left - numberOfPixels;
+            else if (direction == Direction.Down)
+                this.Top = this.Top + numberOfPixels;
+            else
+                throw new Exception("Wtf are you doing passing a nondirectional direction to this method?");
+        }
+
+        private Direction directionTranslator(string checkText)
+        {
+            if (Regex.Match(checkText, "^up.*[0-9]+$").Success)
+                return Direction.Up;
+            else if (Regex.Match(checkText, "^left.*[0-9]+$").Success)
+                return Direction.Left;
+            else if (Regex.Match(checkText, "^right.*[0-9]+$").Success)
+                return Direction.Right;
+            else if (Regex.Match(checkText, "^down.*[0-9]+$").Success)
+                return Direction.Down;
+            else
+                return Direction.Nondirectional;
+        }
+
+        private void displayPreviousDayLog()
+        {
+            var yesterday = DateTime.Now.AddDays(-1);
+            var yesterdaysLog = Path.Combine(Properties.Settings.Default.CommentLogLocation, getCommentLogName(yesterday));
+
+            if(File.Exists(yesterdaysLog))
+                System.Diagnostics.Process.Start("notepad++.exe", yesterdaysLog);            
+        }
+
+        public void InsertLog(string comment)
+        {
+            //Create directory if needed.
+            if (!Directory.Exists(Properties.Settings.Default.CommentLogLocation))
+                Directory.CreateDirectory(Properties.Settings.Default.CommentLogLocation);
+
+            //Write comment to file.
+            using (StreamWriter datFile = new StreamWriter(_todaysLog, File.Exists(_todaysLog)))
+            {
+                datFile.WriteLine(DateTime.Now.ToString("HH:mm:ss") + " - " + comment);
+                datFile.WriteLine();
+            }
+        }
+
+        private void Comments_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            InsertLog("*Application Close*");
+        }
+    }
+}
